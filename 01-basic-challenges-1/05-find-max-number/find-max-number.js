1474390b6414737bab738177d30feabe8727bca0
Felipe <felipe@gooseinsurance.com>
2024-06-17 23:21:54 -0700

    max number challenge

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,14 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+    // Solution with sort()
+    // const result = arr.sort((a, b) => a - b).reverse()
+    // return result[0];
+
+    // Solution with for loop
+    let result = arr[0];
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] > result) { result = arr[i] }
+    }
+    return result;
+}
 
 module.exports = findMaxNumber;
