a5ccebcecde347b9ab977b33555e5e5141d3764b
Felipe <felipe@gooseinsurance.com>
2024-06-17 22:46:18 -0700

    Counter challenge

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,9 @@
-function countOccurrences() {}
+function countOccurrences(word, char) {
+    let count = 0
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === char) { count++ }
+    }
+    return count;
+}
 
 module.exports = countOccurrences;
