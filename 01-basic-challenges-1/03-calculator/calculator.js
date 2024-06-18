ba5aa0fad1168d4c7b235d1803c75877910664c3
Felipe <felipe@gooseinsurance.com>
2024-06-17 22:59:25 -0700

    Calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,8 @@
-function calculator() {}
+function calculator(num1, num2, op) {
+    if (op === '+') return num1 + num2;
+    if (op === '-') return num1 - num2;
+    if (op === '/') return num1 / num2;
+    return num1 * num2
+}
 
 module.exports = calculator;
