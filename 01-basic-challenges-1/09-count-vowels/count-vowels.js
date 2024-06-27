be67dd3c4d3db25b3a6e48feb0d2de7e7b51402e
Felipe <felipe@gooseinsurance.com>
2024-06-26 22:30:32 -0700

    count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,11 @@
-function countVowels() {}
+function countVowels(str) {
+    const newStr = str.toLowerCase();
+    let count = 0
+    const vowels = 'aeiou'
+    for (let i = 0; i < newStr.length; i++) {
+        if (vowels.includes(newStr[i])) count++;
+    }
+    return count;
+}
 
 module.exports = countVowels;
