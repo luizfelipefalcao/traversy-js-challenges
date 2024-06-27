ce2842cbb9917bea3fcd2f10a0b31feb2a648983
Felipe <felipe@gooseinsurance.com>
2024-06-26 22:04:13 -0700

    title case and polidrome

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,13 @@
-function reverseString() {}
+function reverseString(str) {
+    // with loop method
+    const arr = [];
+    for (let i = 0; i < str.length; i++) {
+        arr.unshift(str[i]);
+    }
+    return arr.join('');
+
+    // with built-in method
+    // return str.split('').reverse().join('');
+}
 
 module.exports = reverseString;
--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,15 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    const onlyCharacters = str.replace(/[^a-z0-9]/g, '').toLowerCase()
+    const newStr = [];
+
+    // with for loop method
+    for (let i = 0; i < onlyCharacters.length; i++) {
+        newStr.unshift(onlyCharacters[i])
+    }
+    return newStr.join('') === onlyCharacters;
+
+    // with built-in method
+    // return onlyCharacters.split('').reverse().join('') === onlyCharacters
+}
 
 module.exports = isPalindrome;
