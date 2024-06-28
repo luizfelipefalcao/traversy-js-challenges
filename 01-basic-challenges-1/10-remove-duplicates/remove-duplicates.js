980c91fa8256a45875480fb495684ff92ca9aa93
Felipe <felipe@gooseinsurance.com>
2024-06-27 22:45:28 -0700

    remove duplicates solutioned

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,14 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+    // with Set for loop
+    const newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!newArr.includes(arr[i])) newArr.push(arr[i])
+    }
+    // return newArr;
+
+    // with Set built-in method
+    return Array.from(new Set(arr));
+
+}
 
 module.exports = removeDuplicates;
